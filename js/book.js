547271843a096dc383865c10dc0321bca4fcf6c6
Antar Karmakar <2016-2-60-113@std.ewubd.edu>
2021-09-02 20:18:56 +0600

    edited

--- js/book.js
+++ js/book.js
@@ -12,10 +12,11 @@ const searchBook=()=>{
     const searchField= document.getElementById('search-field');
     const searchText= searchField.value;
     document.getElementById('search-found').innerHTML='';
+    document.getElementById('not-found').innerHTML='';
    
     toggleSpinner('block');
     toggleSearch('none');
-    console.log(searchText);
+   
     searchField.value='';
     const url=`https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
@@ -36,11 +37,16 @@ const displaySearch=books=>{
     searchResult.textContent='';
 
     if(books.docs.length===0){
-        alert('Not Found');
+        const notFoundDiv= document.getElementById('not-found');
+        const h1= document.createElement('h1');
+        h1.classList.add('text-center');
+        h1.style.color='red';
+        h1.innerText='Not Found';
+        notFoundDiv.appendChild(h1);
     }
     
     books.docs.forEach(book=>{
-        console.log(book.author_name);
+        // console.log(book.author_name);
         const div= document.createElement('div');
         
         div.classList.add('col');
@@ -50,15 +56,17 @@ const displaySearch=books=>{
         <img src="https://covers.openlibrary.org/b/id/${book.cover_i? book.cover_i:'10909258'}-M.jpg" class="card-img-top" alt="Picture">
         <div class="card-body">
           <h5 class="card-title">Book Name: ${book.title? book.title:'Not Found'}</h5>
-          <p class="card-text"><strong>Author Name:</strong> ${book.author_name? book.author_name: 'Not Found' }</p>
+          <p class="card-text"><strong>Author Name:</strong> ${book.author_name? book.author_name[0]: 'Not Found' }</p>
           <p class="card-text"><strong>First Publish Year:</strong> ${book.first_publish_year? book.first_publish_year:'Not Found'} </p>
-          <p class="card-text"> <strong>Publisher:</strong> ${book.publisher? book.publisher: 'Not Found'} </p>
+          <p class="card-text"> <strong>Publisher:</strong> ${book.publisher? book.publisher[0]: 'Not Found'} </p>
         </div>
       </div>
         
         `;
         searchResult.appendChild(div);
+       
     });
     toggleSpinner('none');
+    
     toggleSearch('block');
 }
